fcd143112e60f5ae3878f143693f81923961dfd4
NhatDuong1812 <64996930+NhatDuong1812@users.noreply.github.com>
2021-01-21 13:39:52 +0700

    first commit

---
+++ src/main/java/com/calculator/AppInit.java
@@ -0,0 +1,21 @@
+package com.calculator;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{AppConfig.class};
+    }
+    // quy định các lớp cấu hình ứng dụng
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
